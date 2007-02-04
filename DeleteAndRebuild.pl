119ee3d25f6af621310fa76a03a575acc081b13f
Hirotaka Ogawa <hirotaka.ogawa@gmail.com>
2007-02-04 08:42:55 +0000

    Add DeleteAndRebuild plugin to the repository.
    git-svn-id: file:///Users/ogawa/svn/mt-plugin-DeleteAndRebuild/trunk@1 b7bee0e5-6241-4529-aa9e-08ae86e46850

---
+++ DeleteAndRebuild.pl
@@ -0,0 +1,27 @@
+# DeleteAndRebuild
+#
+# $Id$
+
+package MT::Plugin::DeleteAndRebuild;
+use strict;
+use MT 3.3;
+use base qw(MT::Plugin);
+our $VERSION = '0.01';
+
+my $plugin = __PACKAGE__->new({
+    name => 'DeleteAndRebuild',
+    description => 'A plugin for rebuilding archives related to deleted entry.',
+    author_name => 'Hirotaka Ogawa',
+    author_link => 'http://profile.typekey.com/ogawa/',
+    version => $VERSION,
+    callbacks => { 'CMSPostDelete.entry' => \&handler }
+});
+MT->add_plugin($plugin);
+
+sub handler {
+    my ($eh, $app, $obj) = @_;
+    $app->rebuild_entry(Entry => $obj, BuildDependencies => 1);
+    if ($app->config('DeleteFilesAtRebuild')) {
+	$app->publisher->remove_entry_archive_file(Entry => $obj);
+    }
+}
